4d057d3fd51af665bc8982c4ec381aefe1299ae4
John Toriola <john.toriola.tj@gmail.com>
2021-10-18 10:44:07 +0100

    Done with Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Generator from "./components/Generator";
+import data from "./data";
 
 function App() {
+  const [text, setData] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>TIRED OF BORING LOREM IPSUM?</h1>
+      <Generator data={text} />
+    </main>
   );
 }
 
---
+++ src/components/Generator.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const Generator = ({ data }) => {
+  const [toDisplay, setToDisplay] = useState("");
+  const [num, setNum] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let numOfParagraphs = num;
+    if (numOfParagraphs < 0) numOfParagraphs = 1;
+    else if (numOfParagraphs > 8) numOfParagraphs = 8;
+
+    const minData = data.filter((text, index) => {
+      return index < numOfParagraphs;
+    });
+
+    let paragraphs = "";
+    minData.forEach((phrase) => {
+      paragraphs += phrase += "\n\n\n";
+    });
+
+    setToDisplay(paragraphs);
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <label for="num">Paragraphs:</label>
+        <input
+          type="number"
+          id="num"
+          name="num"
+          value={num}
+          onChange={(e) => setNum(e.target.value)}
+        />
+        <button type="submit">Generate</button>
+      </form>
+      <div>{toDisplay}</div>
+    </section>
+  );
+};
+
+export default Generator;
